d643d72882543923603de03f337240926bc99cd8
Michel Falke <michel.falke@webstores.nl>
2020-04-30 11:23:16 +0200

    feature(Daily report): set daily generation and consumption
    This feature is for setting daily variables based on the kWh of consumed and produced power.

---
+++ drivers/ztatz_p1_Daily_Usage/device.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const Homey = require('homey');
+const Device = require('../../lib/Device.js');
+const boolean = require('boolean');
+
+const refreshTimeout = 1000 * 10; // 10 sec
+
+module.exports = class ztatzP1SmartMeterDevice extends Device {
+
+	// this method is called when the Device is inited
+	async _initDevice() {
+		this.log('_initDevice');
+		const device = this.getData();
+
+		// Register flowcard triggers
+		//this._registerFlowCardTriggers();
+
+		// Update server data
+		this._syncDevice();
+
+		// Set update timer
+		this.intervalId = setInterval(this._syncDevice.bind(this), refreshTimeout);
+		this.setSettings({
+			url: device.url,
+		});
+
+		// console.log("register flow triggers");
+	}
+
+	async _deleteDevice() {
+		this.log('_deleteDevice');
+
+		clearInterval(this.intervalId);
+	}
+
+	// Update server data
+	async _syncDevice() {
+		try {
+			let status = await this.api.getPowerGasDay();
+
+			if (status.length != 0) {
+				this.setAvailable();
+
+				let consumptionDelta = status[0].CONSUMPTION_DELTA_KWH;
+				let productionDelta = status[0].PRODUCTION_DELTA_KWH;
+
+				this.changeCapabilityValue('meter_power.consumed_today', Number(consumptionDelta));
+				this.changeCapabilityValue('meter_power.production_today', Number(productionDelta));
+
+			} else {
+				this.setUnavailable('Cannot refresh / Connect');
+			}
+
+
+		} catch (error) {
+			this.error(error);
+			this.setUnavailable(error.message);
+		}
+	}
+};
